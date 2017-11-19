194121073c9375eb142b89e1f0a2b8ce9519f011
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-19 16:25:16 -0500

    add input atoms

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const TextInput = styled.input.attrs({
+  type: 'text',
+})`
+  appearance: none;
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid yellow;
+`;
+
+export const NumberInput = styled(TextInput).attrs({
+  type: 'number',
+})``;
+
+export const Checkbox = styled(TextInput).attrs({
+  type: 'checkbox',
+})``;
+
+export default TextInput;

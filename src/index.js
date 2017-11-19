973a5f641bed24fdaa115dd9f43f31aa1413bedb
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-19 04:16:26 -0500

    paste in subscription tutorial

--- src/index.js
+++ src/index.js
@@ -1,28 +1,71 @@
 import './index.css';
+import './styles/index.css';
 
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink, split } from 'apollo-client-preset';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { WebSocketLink } from 'apollo-link-ws';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
 
+import { GC_AUTH_TOKEN } from './constants';
 import CombatantList from './components/CombatantList';
 import registerServiceWorker from './registerServiceWorker';
 
+const serviceId = 'cja6cc8ww08ny0119esj7ljec';
+
+const httpLink = new HttpLink({
+  uri: `https://api.graph.cool/simple/v1/${serviceId}`,
+});
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader,
+    },
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/${serviceId}`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja6cc8ww08ny0119esj7ljec' }),
-  cache: new InMemoryCache()
+  link,
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
+  <Router>
+    <ApolloProvider client={client}>
       <CombatantList />
-    </Router>
-  </ApolloProvider>,
+    </ApolloProvider>
+  </Router>,
   document.getElementById('root'),
 );
 registerServiceWorker();

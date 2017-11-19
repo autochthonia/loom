54c8d410836f5a4a676701d4e40e148ff7e42700
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-19 04:12:36 -0500

    set up apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+import './index.css';
+
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import CombatantList from './components/CombatantList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja6cc8ww08ny0119esj7ljec' }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <CombatantList />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();

af26d1498ef8a5ba0afb32a34cac3d3497dc2076
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-19 04:35:15 -0500

    update ListCombatants to use subscriptions

--- src/components/CombatantList/CombatantList.jsx
+++ src/components/CombatantList/CombatantList.jsx
@@ -1,10 +1,55 @@
+import { gql } from 'apollo-client-preset';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { map } from 'lodash';
 
 class CombatantList extends Component {
   static defaultProps = {};
   static propTypes = {};
   state = {};
+
+  componentDidMount() {
+    this._subscribeToCombatants();
+  }
+
+  _subscribeToCombatants = () => {
+    this.props.data.subscribeToMore({
+      document: gql`
+        subscription {
+          Combatant {
+            node {
+              id
+              name
+              initiative
+              turnOver
+            }
+          }
+        }
+      `,
+      updateQuery: (
+        previous,
+        {
+          subscriptionData,
+          subscriptionData: { data: { Combatant } },
+          ...rest
+        },
+      ) => {
+        console.log('CombatantList _subscribeToCombatants update:');
+        console.log('previous: \n', previous);
+        console.log('subscriptionData: \n', { ...rest, subscriptionData });
+
+        // replace updated combatant with new one
+        // TODO: what about creation and deletion?
+        // create updateResource, addResource, deleteResource helpers
+        return {
+          ...previous,
+          allCombatants: map(
+            previous.allCombatants,
+            c => (c.id === Combatant.id ? Combatant : c),
+          ),
+        };
+      },
+    });
+  };
 
   render() {
     if (this.props.data && this.props.data.loading) {
@@ -19,8 +64,8 @@ class CombatantList extends Component {
       <div>
         {this.props.data.allCombatants.map(
           ({ id, name, initiative, turnOver }) => (
-            <div>
-              {id} {name} {initiative} {turnOver}
+            <div key={id}>
+              <input type="checkbox" checked={turnOver} /> {name} {initiative}
             </div>
           ),
         )}

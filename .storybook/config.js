070d8f84612b196f2f321f684d06170835e7c89f
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-24 02:34:00 -0500

    update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
-import { configure } from '@storybook/react';
+import 'regenerator-runtime/runtime'
+
+// https://github.com/diegohaz/arc/wiki/Storybook
+import React from 'react';
+import { configure /* addDecorator */ } from '@storybook/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
+// import theme from 'components/themes/default';
+
+const req = require.context('../src', true, /.stories.jsx?$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
